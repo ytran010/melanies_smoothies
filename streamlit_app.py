9dfb41f106e443eabdc3064249de4269e78f17e8
Yvonne Nhat Thao Tran <yvonne.nhat.thao.tran@gmail.com>
2024-11-24 19:56:50 -0800

    Update streamlit_app.py
    changed and added snowflake connection lines 16 and 17

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
 
-
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
