a6d355c640cb225c4187d8f4a072e48eafb76384
Yvonne Nhat Thao Tran <yvonne.nhat.thao.tran@gmail.com>
2024-11-30 11:30:45 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ if ingredients_list:
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
         
-    # st.write(ingredients_string)
+    st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
