ee3029efa05b4c1a258f15edfd8e16a105a9f8ff
Yvonne Nhat Thao Tran <yvonne.nhat.thao.tran@gmail.com>
2024-11-29 20:23:00 -0800

    Update streamlit_app.py
    integrated API response to data frame

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
@@ -35,6 +36,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = true)
         
     # st.write(ingredients_string)
 
@@ -56,8 +59,6 @@ if ingredients_list:
     #     session.sql(my_insert_stmt).collect()
     #     st.success('Your Smoothie is ordered!', icon="✅")
 
-#New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = true)
+
+
+
