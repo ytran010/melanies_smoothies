bcd378d95c63ee58f564cce2e4abc40925500eac
Yvonne Nhat Thao Tran <yvonne.nhat.thao.tran@gmail.com>
2024-11-30 11:41:01 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,8 +61,8 @@ if ingredients_list:
     
     if time_to_insert:
         # session.sql(my_insert_stmt).collect()
-        st.write(my_insert_stmt)
-        st.stop()
+        # st.write(my_insert_stmt)
+        # st.stop()
 
         
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
